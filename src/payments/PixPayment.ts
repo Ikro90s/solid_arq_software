885b4926dc6a970b2ee1d2409351bbaed8c1e546
Icaro Pereira De Paula <136732875+Ikro90s@users.noreply.github.com>
2025-12-29 19:45:35 -0400

    Update PixPayment.ts

--- src/payments/PixPayment.ts
+++ src/payments/PixPayment.ts
@@ -4,6 +4,5 @@ import logger from '../lib/logger';
 export class PixPayment implements IPaymentMethod {
   async process(amount: number, details: any): Promise<void> {
     logger.info(`Processando Pix no valor de R$ ${amount}`);
-    // Lógica fictícia de Pix
   }
 }
